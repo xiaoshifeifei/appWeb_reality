214b37fa6934a0714c8819714b0ad35112e11964
xiaoshifeifei <1521318212@qq.com>
2024-09-11 17:17:47 +0800

    页码增加多语言

--- src/main.js
+++ src/main.js
@@ -1,24 +1,27 @@
-import 'element-plus/es/components/message/style/css'
-import 'element-plus/es/components/loading/style/css'
-import 'element-plus/es/components/notification/style/css'
-import 'element-plus/es/components/message-box/style/css'
-import 'element-plus/theme-chalk/dark/css-vars.css'
-import './style/element_visiable.scss'
+import "element-plus/es/components/message/style/css";
+import "element-plus/es/components/loading/style/css";
+import "element-plus/es/components/notification/style/css";
+import "element-plus/es/components/message-box/style/css";
+import "element-plus/theme-chalk/dark/css-vars.css";
+import "./style/element_visiable.scss";
+import "element-plus/dist/index.css";
+import zhCn from "element-plus/es/locale/lang/zh-cn";
+import ElementPlus from "element-plus";
 
-import { createApp } from 'vue'
+import { createApp } from "vue";
 // 引入gin-vue-admin前端初始化相关内容
-import './core/gin-vue-admin'
+import "./core/gin-vue-admin";
 // 引入封装的router
-import router from '@/router/index'
-import '@/permission'
-import run from '@/core/gin-vue-admin.js'
-import auth from '@/directive/auth'
-import { store } from '@/pinia'
-import App from './App.vue'
-import i18n from './i18n' // added by mohamed hassan to multilangauge
+import router from "@/router/index";
+import "@/permission";
+import run from "@/core/gin-vue-admin.js";
+import auth from "@/directive/auth";
+import { store } from "@/pinia";
+import App from "./App.vue";
+import i18n from "./i18n"; // added by mohamed hassan to multilangauge
 
-const app = createApp(App)
-app.config.productionTip = false
+const app = createApp(App);
+app.config.productionTip = false;
 
 app
   .use(run)
@@ -26,6 +29,7 @@ app
   .use(auth)
   .use(router)
   .use(i18n)
-  .mount('#app')
+  .use(ElementPlus, { locale: zhCn })
+  .mount("#app");
 
-export default app
+export default app;
