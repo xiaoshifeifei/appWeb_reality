4fe5eb2b5cd8aad41b0422a3e09a0c2e9959f3df
xiaoshifeifei <1521318212@qq.com>
2024-11-04 09:43:56 +0800

    1

--- src/components/dataTime/timePickMinute.vue
+++ src/components/dataTime/timePickMinute.vue
@@ -29,7 +29,7 @@ const defaultTime = [
   new Date(2000, 1, 1, 0, 0, 0),
   new Date(2000, 2, 1, 23, 59, 59, 999),
 ];
-const emits = defineEmits(["update:modelValue"]);
+const emits = defineEmits(["update:modelValue", "close"]);
 
 const props = defineProps({
   modelValue: {
