d73da65c664d5eb5f87c637efcbf31c3a7b2ae86
xiaoshifeifei <1521318212@qq.com>
2024-12-05 15:53:33 +0800

    1

---
+++ src/view/setting/notice/index.vue
@@ -0,0 +1,396 @@
+<template>
+  <div>
+    <div class="gva-search-box">
+      <el-form ref="searchForm" :inline="true" :model="searchInfo">
+        <!-- <el-form-item label="code">
+            <el-input v-model="searchInfo.key" placeholder="code" />
+          </el-form-item> -->
+        <el-form-item>
+          <el-button type="primary" icon="search" @click="onSubmit">
+            {{ t("general.search") }}
+          </el-button>
+          <el-button icon="refresh" @click="onReset">
+            {{ t("general.reset") }}
+          </el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+    <div class="gva-table-box">
+      <div class="gva-btn-list">
+        <el-button type="primary" icon="plus" @click="openDialog('add')">
+          {{ t("general.add") }}
+        </el-button>
+      </div>
+      <el-table
+        border
+        :data="tableData"
+        @selection-change="handleSelectionChange"
+        highlight-current-row
+        :header-cell-style="{
+          backgroundColor: 'var(--el-tab-bgc)',
+          Color: '#FFF',
+        }"
+        :row-class-name="tableRowClassName"
+      >
+        <el-table-column type="selection" align="center" width="60" />
+        <el-table-column
+          align="center"
+          :label="t('tableColumn.code')"
+          min-width="150"
+          prop="code"
+        />
+        <el-table-column
+          align="center"
+          :label="t('tableColumn.desc')"
+          min-width="170"
+          prop="desc"
+        >
+        </el-table-column>
+        <el-table-column
+          align="center"
+          :label="t('tableColumn.created')"
+          min-width="200"
+          prop="created"
+        >
+          <template #default="scope">
+            <div>{{ dataGet(scope.row.created) }}</div>
+          </template>
+        </el-table-column>
+
+        <el-table-column
+          align="center"
+          fixed="right"
+          :label="t('general.operations')"
+          min-width="200"
+        >
+          <template #default="scope">
+            <el-button
+              type="primary"
+              size="small"
+              icon="edit"
+              @click="editTackFunc(scope.row)"
+            >
+              {{ t("general.edit") }}
+            </el-button>
+            <el-button
+              size="small"
+              icon="delete"
+              @click="deleteTackFunc(scope.row)"
+            >
+              {{ t("general.delete") }}
+            </el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <div class="gva-pagination">
+        <el-pagination
+          :current-page="page"
+          :page-size="pageSize"
+          :page-sizes="[10, 30, 50, 100]"
+          :total="total"
+          layout="total, sizes, prev, pager, next, jumper"
+          @current-change="handleCurrentChange"
+          @size-change="handleSizeChange"
+        />
+      </div>
+    </div>
+
+    <el-drawer
+      v-if="dialogFormVisible"
+      v-model="dialogFormVisible"
+      size="60%"
+      :before-close="closeDialog"
+      :show-close="false"
+    >
+      <template #header>
+        <div class="flex justify-between items-center">
+          <span class="text-lg">{{ dialogTitle }}</span>
+          <div>
+            <el-button @click="closeDialog">
+              {{ t("general.close") }}
+            </el-button>
+            <el-button type="primary" @click="enterDialog">
+              {{ t("general.confirm") }}
+            </el-button>
+          </div>
+        </div>
+      </template>
+
+      <el-form
+        class="myForm"
+        ref="apiForm"
+        :model="form"
+        :rules="rules"
+        label-width="80px"
+      >
+        <el-form-item :label="t('tableColumn.code')" prop="code">
+          <el-input
+            v-model="form.code"
+            :disabled="type === 'edit'"
+            autocomplete="off"
+          />
+        </el-form-item>
+        <el-form-item :label="t('tableColumn.desc')" prop="desc">
+          <el-input v-model="form.desc" autocomplete="off" />
+        </el-form-item>
+      </el-form>
+    </el-drawer>
+  </div>
+</template>
+  
+  <script setup>
+import {
+  virtualItemGetList,
+  virtualItemDel,
+  virtualItemEdit,
+  virtualItemAdd,
+} from "@/api/tack";
+import { ref } from "vue";
+import { ElMessage, ElMessageBox } from "element-plus";
+import { useRouter } from "vue-router";
+import { useI18n } from "vue-i18n"; // added by mohamed hassan to support multilanguage
+const { t } = useI18n(); // added by mohamed hassan to support multilanguage
+const router = useRouter();
+
+defineOptions({
+  name: "notice",
+});
+
+const apis = ref([]);
+const form = ref({
+  code: "",
+  desc: "",
+});
+
+const type = ref("");
+const rules = ref({
+  code: [{ required: true, message: "请输入code", trigger: "blur" }],
+  desc: [{ required: true, message: "请输入desc", trigger: "blur" }],
+});
+
+const page = ref(1);
+const total = ref(0);
+const pageSize = ref(10);
+const tableData = ref([]);
+const searchInfo = ref({});
+
+const onReset = () => {
+  searchInfo.value = {};
+};
+// 搜索
+
+const onSubmit = () => {
+  page.value = 1;
+  pageSize.value = 10;
+  getTableData();
+};
+
+// 分页
+const handleSizeChange = (val) => {
+  pageSize.value = val;
+  getTableData();
+};
+
+const dataGet = (dateStr) => {
+  let date = new Date(dateStr);
+  let formattedDate =
+    date.getFullYear() +
+    "-" +
+    (date.getMonth() + 1).toString().padStart(2, "0") +
+    "-" +
+    date.getDate().toString().padStart(2, "0") +
+    " " +
+    date.getHours().toString().padStart(2, "0") +
+    ":" +
+    date.getMinutes().toString().padStart(2, "0") +
+    ":" +
+    date.getSeconds().toString().padStart(2, "0");
+  return formattedDate;
+};
+
+const handleCurrentChange = (val) => {
+  page.value = val;
+  getTableData();
+};
+
+// 查询
+const getTableData = async () => {
+  const table = await virtualItemGetList({
+    page: page.value,
+    pageSize: pageSize.value,
+    ...searchInfo.value,
+  });
+  if (table.code === 0) {
+    tableData.value = table.data.list;
+    total.value = table.data.total;
+    page.value = table.data.page;
+    pageSize.value = table.data.pageSize;
+  }
+};
+
+getTableData();
+// 批量操作
+const handleSelectionChange = (val) => {
+  apis.value = val;
+};
+
+const syncApiData = ref({
+  newApis: [],
+  deleteApis: [],
+  ignoreApis: [],
+});
+
+// 弹窗相关
+const apiForm = ref(null);
+const initForm = () => {
+  apiForm.value.resetFields();
+  form.value = {
+    code: "",
+    desc: "",
+  };
+};
+
+const dialogTitle = ref(t("general.add"));
+const dialogFormVisible = ref(false);
+const openDialog = (key) => {
+  switch (key) {
+    case "add":
+      dialogTitle.value = t("general.add");
+      break;
+    case "edit":
+      dialogTitle.value = t("general.edit");
+      break;
+    default:
+      break;
+  }
+  type.value = key;
+  dialogFormVisible.value = true;
+};
+const closeDialog = () => {
+  initForm();
+  dialogFormVisible.value = false;
+};
+
+const editTackFunc = async (row) => {
+  let rows = JSON.parse(JSON.stringify(row));
+  form.value = rows;
+  openDialog("edit");
+};
+
+const enterDialog = async () => {
+  apiForm.value.validate(async (valid) => {
+    if (valid) {
+      switch (type.value) {
+        case "add":
+          {
+            const res = await virtualItemAdd(form.value);
+            if (res.code === 0) {
+              ElMessage({
+                type: "success",
+                message: t("user.userAddedNote"),
+                showClose: true,
+              });
+              getTableData();
+              closeDialog();
+            }
+          }
+          break;
+        case "edit":
+          {
+            const res = await virtualItemEdit(form.value);
+            if (res.code === 0) {
+              ElMessage({
+                type: "success",
+                message: t("user.userEditedNote"),
+                showClose: true,
+              });
+              getTableData();
+              closeDialog();
+            }
+          }
+          break;
+        default:
+          {
+            ElMessage({
+              type: "error",
+              message: t("view.api.unknownOperation"),
+              showClose: true,
+            });
+          }
+          break;
+      }
+    }
+  });
+};
+
+const deleteTackFunc = async (row) => {
+  ElMessageBox.confirm(t("general.deleteConfirm"), t("general.hint"), {
+    confirmButtonText: t("general.confirm"),
+    cancelButtonText: t("general.cancel"),
+    type: "warning",
+  }).then(async () => {
+    const res = await virtualItemDel({ code: row.code });
+    if (res.code === 0) {
+      ElMessage({
+        type: "success",
+        message: t(`general.deleteSuccess`),
+      });
+      getTableData();
+    }
+  });
+};
+const tableRowClassName = ({ row, rowIndex }) => {
+  if (rowIndex % 2 == 0) {
+    return "";
+  } else {
+    return "warnBg";
+  }
+};
+</script>
+  <style scoped lang="scss">
+.light .gva-table-box {
+  ::v-deep(.el-table tr th .cell) {
+    color: var(--el-bg-font-color) !important;
+  }
+  ::v-deep(.el-table__row.warnBg .el-table__cell) {
+    width: 100%;
+    height: 100%;
+    background-color: var(--el-warn-bgc);
+    color: #000;
+  }
+  ::v-deep(.el-table__body tr:hover > td) {
+    background-color: var(--el-warn-bgc-hover) !important;
+  }
+  ::v-deep(.el-table__body tr.current-row > td) {
+    background-color: var(--el-warn-bgc-current) !important;
+  }
+}
+
+.dark .gva-table-box {
+  ::v-deep(.el-table tr th .cell) {
+    color: var(--el-bg-font-color) !important;
+  }
+  ::v-deep(.el-table__row.warnBg .el-table__cell) {
+    width: 100%;
+    height: 100%;
+    background-color: var(--el-warn-bgc);
+    color: #000;
+  }
+  ::v-deep(.el-table__body tr:hover > td) {
+    background-color: var(--el-warn-bgc-hover) !important;
+  }
+  ::v-deep(.el-table__body tr.current-row > td) {
+    background-color: var(--el-warn-bgc-current) !important;
+  }
+}
+</style>
+  
+  <style scoped lang="scss">
+.warning {
+  color: #dc143c;
+}
+.el-input-number {
+  width: 50%;
+}
+</style>
+  
